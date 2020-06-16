9edeafd747339adc691f5d4931600ef0551cc3d5
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-06-16 14:01:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CSC_205_CH9_part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("called before MyMethod");
+                MyMethod();
+                Console.WriteLine("called after MyMethod");
+
+                //StreamReader inFile = new StreamReader("input.txt");
+                //Console.WriteLine(inFile.ReadLine());
+
+                //inFile.Close();
+
+            }
+
+            catch (FileNotFoundException e)
+            {
+                // recovery code
+                Console.WriteLine("File not found" + e.Message);
+                throw; //re-throw
+            }
+
+            catch (Exception e)
+            {
+                // recovery code
+                Console.WriteLine("catch them all" + e.Message);
+
+            }
+
+            finally
+            {
+                Console.WriteLine("I am always gettingn called");
+            }
+
+            Console.WriteLine("The End");
+            
+        }
+
+        static void MyMethod()
+        {
+            throw new DivideByZeroException();
+            //throw new DivideByZeroException();
+            throw new FileNotFoundException();
+
+        }
+
+        static int FutureMethod()
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+
+}
